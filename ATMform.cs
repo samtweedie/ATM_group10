9eb4c7b79c435931519bd863651d3ad9ed5d5261
samtweedie <76115945+samtweedie@users.noreply.github.com>
2023-03-19 15:48:32 +0000

    Clears textbox on incorrect inputs

--- ATMform.cs
+++ ATMform.cs
@@ -27,28 +27,24 @@ namespace ATM
 
         private void Continue_Click(object sender, EventArgs e)
         {
-            if (next == 1)
-            {
-                this.Visible = false;
-                Options options = new Options(activeAccount, atm);  
-                options.Show();
-            }
             if (!accountCorrect) //account screen
             {
                 if (InputDetails.Text.Length < 6)
                 {
+                    InputDetails.Clear();
                     MessageBox.Show("You have entered less than 6 digits. Your account number must be 6 digits. Please try again", "Error", MessageBoxButtons.OK);
                 }
                 else
                 {
-                   
                     activeAccount = atm.findAccount(Convert.ToInt32(InputDetails.Text));
                     if (activeAccount == null)
                     {
+                        InputDetails.Clear();
                         MessageBox.Show("The account number you entered could not be found. Please try again", "Error", MessageBoxButtons.OK);
                     }
                     else //changes form to ask for pin
                     {
+                        InputDetails.Clear();
                         accountCorrect = true;
                         label1.Text = "Please enter pin";
                         InputDetails.MaxLength = 4;
@@ -59,13 +55,21 @@ namespace ATM
             {
                 if (InputDetails.Text.Length < 4)
                 {
+                    InputDetails.Clear();
                     MessageBox.Show("You have entered less than 4 digits. Your pin must be 4 digits. Please try again", "Error", MessageBoxButtons.OK);
                 }
                 else
                 {
                     if (activeAccount.checkPin(Convert.ToInt32(InputDetails.Text)))
                     {
-                        next = 1;
+                        this.Visible = false;
+                        Options options = new Options(activeAccount, atm);
+                        options.Show();
+                    }
+                    else
+                    {
+                        InputDetails.Clear();
+                        MessageBox.Show("Incorrect pin. Please try again", "Error", MessageBoxButtons.OK);
                     }
                 }
             }
